a582e91a3d0198442d8f65cc1e276bbc8ec222f0
Shardul Nalegave <nalegaveshardul40@gmail.com>
2023-03-14 23:54:57 +0530

    Basic rendering

--- examples/basic_neural_net.rs
+++ examples/basic_neural_net.rs
@@ -5,6 +5,7 @@ use nannou::prelude::*;
 fn main() {
   nannou::app(model)
     .event(event)
+    .update(update)
     .simple_window(view)
     .run();
 }
@@ -16,6 +17,10 @@ fn model(app: &App) -> InteractiveMode {
 
 fn event(app: &App, model: &mut InteractiveMode, event: Event) {
   model.event(app, event);
+}
+
+fn update(app: &App, model: &mut InteractiveMode, update: Update) {
+  model.update(app, update);
 }
 
 fn view(app: &App, model: &InteractiveMode, frame: Frame) {
--- src/interactive.rs
+++ src/interactive.rs
@@ -14,10 +14,13 @@ impl InteractiveMode {
     app.main_window().set_maximized(true);
     app.main_window().set_resizable(false);
     app.main_window().set_title("Nexus - Interactive Mode");
-    Self {
-      net,
-      origin: Point2::new(0.0, 0.0),
-    }
+
+    let origin = Point2::new(0.0, 0.0);
+    Self { net, origin }
+  }
+
+  pub fn update(&mut self, _app: &App, _update: Update) {
+    //
   }
 
   pub fn event(&mut self, _app: &App, event: Event) {
@@ -46,36 +49,55 @@ impl InteractiveMode {
     let draw = app.draw();
     draw.background().rgb(0.1, 0.1, 0.1);
 
-    let mut pos = self.origin;
-    pos.y = -(100.0*(self.net.input_size as f32 - 1.0))/2.0;
+    let draw_origin = self.origin + Point2::new(app.window_rect().left() + 100.0, 0.0);
+    let mut current_layer_positions: Vec<Point2> = vec![];
+
+    let mut pos = draw_origin;
+    pos.y -= (100.0*(self.net.input_size as f32 - 1.0))/2.0;
     for _ in 0..self.net.input_size {
       draw.ellipse()
         .xy(pos)
         .w_h(50.0, 50.0)
         .rgb(0.5, 0.5, 0.5);
 
+      current_layer_positions.push(pos);
+
       pos.y += 100.0;
     }
+    let mut prev_layer_positions = current_layer_positions;
+    current_layer_positions = vec![];
 
-    let mut pos_layer = self.origin + Point2::new(150.0, 0.0);
+    let mut pos_layer = draw_origin + Point2::new(150.0, 0.0);
     for layer in &self.net.layers {
-      pos_layer.y = -(100.0*(layer.num_neurons as f32 - 1.0))/2.0;
+      let vertical_shift = (100.0*(layer.num_neurons as f32 - 1.0))/2.0;
+      pos_layer.y -= vertical_shift;
       for i in 0..layer.num_neurons {
         let mut pos = pos_layer;
         pos.y += (i as f32)*100.0;
+        current_layer_positions.push(pos);
 
         draw.ellipse()
           .xy(pos)
           .w_h(50.0, 50.0)
           .rgb(0.5, 0.5, 0.8);
+
+        for pt in &prev_layer_positions {
+          draw.line()
+            .start(*pt)
+            .end(pos)
+            .weight(2.0)
+            .rgb(0.0, 0.0, 0.0);
+        }
       }
 
-      pos_layer.y = 0.0;
+      pos_layer.y += vertical_shift;
       pos_layer.x += 150.0;
+      prev_layer_positions = current_layer_positions;
+      current_layer_positions = vec![];
     }
 
     draw.ellipse()
-      .xy(self.origin)
+      .xy(draw_origin)
       .w_h(10.0, 10.0)
       .rgb(1.0, 1.0, 1.0);
 
